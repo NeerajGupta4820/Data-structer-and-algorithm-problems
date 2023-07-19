5ec5ae95133e0673d82b8485ca658a40824dafaa
Neeraj Gupta <105665359+NeerajGupta4820@users.noreply.github.com>
2023-07-19 17:06:41 +0530

    Update binarysearch.cpp
    searching of element using recursion in binary array

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -4,6 +4,7 @@ using namespace std;
 bool binarysearch(int *arr,int s,int e,int k){
     //base case
     if(s>e) return false;
+    if(arr[mid]==k) return true;
     int mid=s+(e-s)/2;
     if(arr[mid]<k) return binarysearch(arr,mid+1,e,k);
     else return binarysearch(arr,s,mid-1,k);
