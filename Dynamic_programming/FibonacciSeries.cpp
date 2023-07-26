bb8915d32a58dbd14a912b4fdd667358312aacca
Neeraj Gupta <105665359+NeerajGupta4820@users.noreply.github.com>
2023-07-26 13:24:09 +0530

    FibonacciSeries.cpp
    dp problem

---
+++ Dynamic_programming/FibonacciSeries.cpp
@@ -0,0 +1,19 @@
+/*************************FibonacciSeries ************************/
+#include<iostream>
+using namespace std;
+int fib(int n,vector<int>&dp){
+  if(n<=1) return n;
+  if(dp[n]!=-1) return dp[n];
+  dp[n]=fib(n-1)+fib(n-2);
+  return dp[n];
+}  
+int main(){
+  int n;
+  cin>>n;
+  vector<int>dp(n+1);
+  for(int i=0;i<=n;i++){
+    dp[i]=-1;
+  }
+  cout<<fib(n,dp)<<endl;
+  return 0;
+}
